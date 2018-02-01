e4aa13484144e4669bf7a530fc3451044ee3a3c7
Lucas Felipe <30655133+luksave@users.noreply.github.com>
2018-02-01 13:39:55 -0200

    Delete: Removendo arquivo desnecessário no exemplo

--- Exemplos/CMake - Configure/config/sistema.cmake
+++
@@ -1,5 +0,0 @@
-#Config.sistema - Versão mínima do CMake: 3.0
-cmake_minimum_required (VERSION 3.0)
-
-#Verifica que o script foi aberto
-message(STATUS "Prefixo de instalação: " ${CMAKE_INSTALL_PREFIX} )
