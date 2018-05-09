9c83bf0bc3d8584fa3127513fbd852bb6f73830a
Lucas Felipe <30655133+luksave@users.noreply.github.com>
2018-05-09 14:45:50 -0300

    Tst: coverage de argv

--- Exemplos/CMake/CMake-Tests/tests/main.c
+++ Exemplos/CMake/CMake-Tests/tests/main.c
@@ -4,8 +4,24 @@ int main(int argc, char *argv[]){
 	
 	int x; 
 	int y;
+	int num  = argc - 1;
+  	int test = atoi(argv[1]);
 	
-	int test = atoi(argv[1]);
+	if (num == 0) 
+  		printf("Nenhum argumento fornecido\n");
+		
+	else if (num == 0) // Erro intensional
+		printf("1 argumento fornecido\n");
+	
+	else if (num == 2)
+		printf("2 argumentos fornecidos\n");
+	
+	else 
+		printf("%d argumentos fornecidos\n", num);
+	
+	if (argv != 0) {
+		printf("argv nao eh nulo\n");; // Ponto e virgula extra intencional
+	}
 
 	x = atoi(argv[2]);
 	y = atoi(argv[3]);
